616589f01a64ae7d4ae9d25552c65adabf67e472
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2019-06-05 14:52:54 +0100

    grid bug fix

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,58 @@
 import React from "react";
 
 function Features() {
-  return <section className="features">Features</section>;
+  return (
+    <section className="features">
+      <div class="feature">
+        <img src="images/placeholder.svg" alt="Placeholder" />
+        <h4 class="heading-4 heading-4--dark">World's best luxury homes</h4>
+        <p class="feature__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+      <div class="feature">
+        <img src="images/locked.svg" alt="Svg here" />
+        <h4 class="heading-4 heading-4--dark">World's best luxury homes</h4>
+        <p class="feature__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+      <div class="feature">
+        <img src="images/bar-chart.svg" alt="Svg here" />
+        <h4 class="heading-4 heading-4--dark">World's best luxury homes</h4>
+        <p class="feature__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+      <div class="feature">
+        <img src="images/trophy.svg" alt="Svg here" />
+        <h4 class="heading-4 heading-4--dark">World's best luxury homes</h4>
+        <p class="feature__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+      <div class="feature">
+        <img src="images/world.svg" alt="Svg here" />
+        <h4 class="heading-4 heading-4--dark">World's best luxury homes</h4>
+        <p class="feature__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+      <div class="feature">
+        <img src="images/employee.svg" alt="Svg here" />
+        <h4 class="heading-4 heading-4--dark">World's best luxury homes</h4>
+        <p class="feature__text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Features;
