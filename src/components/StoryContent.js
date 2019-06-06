0a9522e3b6eb9002cbec43004918f3acd24f8025
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2019-06-06 08:31:17 +0100

    feature bug fix

--- src/components/StoryContent.js
+++ src/components/StoryContent.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function StoryContent() {
-  return <div className=" story-content">Content here </div>;
+  return (
+    <div className=" story-content">
+      <h3 className="heading-3">HAPPY CUSTOMERS</h3>
+      <h2 className="heading-2 heading-2-dark">
+        &ldquo; The best decision of our lives &rdquo;{" "}
+      </h2>
+      <p className="story-text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+        distinctio necessitatibus pariatur voluptatibus. Quidem consequatur
+        harum volupta!{" "}
+      </p>
+      <button className="btn">Find your own home</button>
+    </div>
+  );
 }
 
 export default StoryContent;
