d8408f6f81a16b6c02db8bc37140e0a1cfb900a9
joye <shonubij@gmail.com>
2019-06-07 12:35:47 +0100

    story section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Main() {
-  return <main>Main</main>;
+  return <main>
+  <h3 className="heading-3 heading-3-dark">Your own home</h3>
+  <h1 className="heading-1">The ultimate personal freedom</h1>
+  <button className= "btn">View our properties</button>
+
+  <div className="header-seenon">Seen on</div>
+  <div className="header-seenon-logos">
+  <img src="images/logo-bbc.png" alt="Seen on logo 1"/>
+  <img src="images/logo-forbes.png" alt="Seen on logo 2" />
+  <img src="images/logo-techcrunch.png" alt="Seen on logo 3" />
+  <img src="images/logo-bi.png" alt="Seen on logo 4" />
+  </div>
+
+  </main>;
 }
 
 export default Main;
--- src/components/StoryPicture.js
+++ src/components/StoryPicture.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 function StoryPicture() {
-  return <div className="story-picture">Picture here</div>;
+  return <div className="story-picture">
+  <img src="images/story-1.jpeg" alt="Seen on logo 1" className="story-picture-1"/>
+  <img src="images/story-2.jpeg" alt="Seen on logo 1" className="story-picture-2"/>
+  </div>;
 }
 
 export default StoryPicture;
